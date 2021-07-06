e8cdffd77c2787c2a4cee904a400c935ff9ca04a
Davi Leal <lealm.davi@gmail.com>
2021-07-06 18:21:02 -0300

    refactor: firestore search improvements

--- lib/views/ListChatScreen/ListChatScreen.dart
+++ lib/views/ListChatScreen/ListChatScreen.dart
@@ -1,41 +1,10 @@
+import 'package:beco_driver/views/ListChatScreen/widgets/GetRoutePassengers.dart';
 import 'package:flutter/material.dart';
 
 import 'package:beco_driver/core/core.dart';
-import 'package:beco_driver/views/ListChatScreen/widgets/ListTileWidget.dart';
 
 class ListChatScreen extends StatefulWidget {
-  final String driverName;
-  final String driverUid;
-  final String firstPassenger;
-  final String firstPassengerImageUrl;
-  final String firstPassengerUid;
-  final String secondPassenger;
-  final String secondPassengerImageUrl;
-  final String secondPassengerUid;
-  final String thirdPassenger;
-  final String thirdPassengerImageUrl;
-  final String thirdPassengerUid;
-  final String fourthPassenger;
-  final String fourthPassengerImageUrl;
-  final String fourthPassengerUid;
-
-  ListChatScreen(
-    this.driverName,
-    this.driverUid,
-    this.firstPassenger,
-    this.firstPassengerImageUrl,
-    this.firstPassengerUid,
-    this.secondPassenger,
-    this.secondPassengerImageUrl,
-    this.secondPassengerUid,
-    this.thirdPassenger,
-    this.thirdPassengerImageUrl,
-    this.thirdPassengerUid,
-    this.fourthPassenger,
-    this.fourthPassengerImageUrl,
-    this.fourthPassengerUid, {
-    Key? key,
-  }) : super(key: key);
+  ListChatScreen({Key? key}) : super(key: key);
 
   @override
   _ListChatScreenState createState() => _ListChatScreenState();
@@ -47,6 +16,7 @@ class _ListChatScreenState extends State<ListChatScreen> {
     final double widthMargin = MediaQuery.of(context).size.width / 40;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: Padding(
           padding: EdgeInsets.all(20),
@@ -67,72 +37,25 @@ class _ListChatScreenState extends State<ListChatScreen> {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.only(left: widthMargin),
-          child: ListView(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: widthMargin * 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Chat", style: AppTextStyles.upTitle),
-                    SizedBox(height: 5),
-                    Container(
-                      width: 30,
-                      height: 2,
-                      color: Colors.blue,
-                    ),
-                    SizedBox(height: 30),
-                    ListTileWidget(
-                      widget.firstPassenger,
-                      widget.driverName,
-                      "Vou atrasar um pouco",
-                      widget.firstPassengerImageUrl,
-                      "3:30 AM",
-                      1,
-                      widget.driverUid,
-                      widget.firstPassengerUid,
-                      key: ValueKey(widget.firstPassengerUid),
-                    ),
-                    SizedBox(height: 15),
-                    ListTileWidget(
-                      widget.secondPassenger,
-                      widget.driverName,
-                      "Cheguei!",
-                      widget.secondPassengerImageUrl,
-                      "3:30 AM",
-                      3,
-                      widget.driverUid,
-                      widget.secondPassengerUid,
-                      key: ValueKey(widget.secondPassengerUid),
-                    ),
-                    SizedBox(height: 15),
-                    ListTileWidget(
-                      widget.thirdPassenger,
-                      widget.driverName,
-                      "Nova mensagem",
-                      widget.thirdPassengerImageUrl,
-                      "3:30 AM",
-                      0,
-                      widget.driverUid,
-                      widget.thirdPassengerUid,
-                      key: ValueKey(widget.thirdPassengerUid),
-                    ),
-                    SizedBox(height: 15),
-                    ListTileWidget(
-                      widget.fourthPassenger,
-                      widget.driverName,
-                      "Nova mensagem",
-                      widget.fourthPassengerImageUrl,
-                      "3:30 AM",
-                      0,
-                      widget.driverUid,
-                      widget.fourthPassengerUid,
-                      key: ValueKey(widget.fourthPassengerUid),
-                    ),
-                  ],
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: widthMargin * 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Chat", style: AppTextStyles.upTitle),
+                SizedBox(height: 5),
+                Container(
+                  width: 30,
+                  height: 2,
+                  color: Colors.blue,
                 ),
-              ),
-            ],
+                SizedBox(height: 30),
+                Container(
+                  height: MediaQuery.of(context).size.height / 1.5,
+                  child: GetRoutePassengers(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/views/ListChatScreen/widgets/GetRoutePassengers.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:beco_driver/views/ListChatScreen/widgets/ListTileWidget.dart';
+
+class GetRoutePassengers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference routes =
+        FirebaseFirestore.instance.collection('routes');
+
+    final String selectedRoute = "Gwo9mVet7JJMi2Je8yRw";
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: routes.doc(selectedRoute).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          return ListView.builder(
+            itemCount: data['passengersList'].length,
+            itemBuilder: (context, index) => ListTileWidget(
+              data['passengersList'][index]['name'],
+              data['driverName'],
+              "Envie uma mensagem!",
+              data['passengersList'][index]['imageUrl'],
+              "3:30 AM",
+              1,
+              data['driverUid'],
+              data['passengersList'][index]['uid'],
+              key: ValueKey(data['passengersList']),
+            ),
+          );
+        }
+
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
